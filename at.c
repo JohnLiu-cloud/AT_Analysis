aaf55de89d1527480431e60f2a6e5e46376231c3
JohnLiu-cloud <646197300@qq.com>
2022-01-04 09:53:12 +0800

    use stm32

--- at.c
+++ at.c
@@ -1,9 +1,12 @@
-#include "driverlib.h"
+#include "stm32f10x.h" 
 #include "stdio.h"
-#include "uart3.h"
-#include "at.h"
+#include <string.h>
+#include "app_uart.h"
+#include "usart.h"
 
-char at_cmd_list[][32] = { AT , AT_CGMI , AT_UPSV , AT_CGSN , AT_CCID  , AT_CFUN , AT_UMNOPROF , AT_CFUN_1 };
+at_state_list_t g_at_state;
+
+char at_cmd_list[][32] = { AT , AT_MANU_ID , AT_IMEI , AT_CCID , AT_NORMAL , AT_RSSI };
 
 static void my_delay(uint32_t x);
 
@@ -39,23 +42,20 @@ static void at_data_analysis(  int8_t *cmd , uint8_t *p_data , uint16_t len )
 {
     char *p_rx;
     char send[100];
+		p_rx = (char*)p_data;
     switch( *cmd )
     {
         case GET_AT:
+						//printf(p_rx);
             break;
-        case GET_CGMI:
+        case GET_MANU_ID:
+						//printf(p_rx);
             break;
-        case GET_UPSV:
+        case GET_IMEI:
+						//printf(p_rx);
             break;
         case GET_ICCID:
-            break;
-        case GET_CPIN:
-            break;
-        case SET_UMNOPROF:
-            break;
-        case GET_CFUN:
-            break;
-        case SET_CFUN_1:
+						//printf(p_rx);
             break;
     }
 }
@@ -69,77 +69,120 @@ void at_cmd_send(  int8_t *cmd , uint8_t *p_data , uint16_t len )
     {
       return;
     }
-    memset(rx_buff , 0 , RX_BUFF_SIZE);
+		memset( send , 0 , 100 );
+    memset(rx1_buff , 0 , RX_BUFF_SIZE);
     switch( *cmd )
     {
         case GET_AT:
             my_delay(200);
-            sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_CGMI]);
-            uart3_send_data((uint8_t*)send,strlen(send));
+						g_at_state = STATE_POWER_ON;
+            sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_MANU_ID]);
+						USART2_printf( send );
             break;
-        case GET_CGMI:
+        case GET_MANU_ID:
             my_delay(200);
-            sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_UPSV]);
-            uart3_send_data((uint8_t*)send,strlen(send));
+            sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_IMEI]);
+						USART2_printf( send );
             break;
-        case GET_UPSV:
+        case GET_IMEI:
             my_delay(200);
             sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_ICCID]);
-            uart3_send_data((uint8_t*)send,strlen(send));
+						USART2_printf( send );
             break;
         case GET_ICCID:
             my_delay(200);
-            sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_CPIN]);
-            uart3_send_data((uint8_t*)send,strlen(send));
+            sprintf(send,"%s\r\n",(char*)&at_cmd_list[SET_NORMAL]);
+						USART2_printf( send );					
             break;
-        case GET_CPIN:
-            my_delay(200);
-            sprintf(send,"%s\r\n",(char*)&at_cmd_list[SET_UMNOPROF]);
-            uart3_send_data((uint8_t*)send,strlen(send));
-            break;
-        case SET_UMNOPROF:
-            break;
-        case GET_CFUN:
-            break;
-        case SET_CFUN_1:
-            break;
+				case SET_NORMAL:
+						g_at_state = STATE_GET_RSSI;
+					break;
+				case GET_RSSI:
+
+					break;
+
     }
 }
 
-void at_handle(void)
+void at_cmd_rssi(void)
+{
+	char send[100];
+	memset( send , 0 , 100 );
+	sprintf(send,"%s\r\n",(char*)&at_cmd_list[GET_RSSI]);
+	USART2_printf( send );		
+}
+
+void at_cmd_pwron(void)
+{
+	USART2_printf("AT\r\n");
+}
+
+
+void uart2_at_handle(void)
 {
     char *p_rx;
 
     int8_t cmd=-1;
 
     uint16_t len=0;
+	
+		uint16_t i;
+	
+		uint8_t buf[100];
 
-    if( rx_cnt_in != 0 )
+    if( rx2_cnt_in != 0 )
     {
-        while(rx_cnt_in>rx_cnt_out)
+				
+        while(rx2_cnt_in>rx2_cnt_out)
         {
-            p_rx = strstr((char *)&rx_buff[rx_cnt_out], "\r\n");
+						//p_rx = strstr((char *)&rx2_buff[rx2_cnt_out], "\r\r\n");
+					
+						p_rx = strstr((char *)&rx2_buff[rx2_cnt_out], "\r\n");
+            
             if(p_rx !=  NULL)
             {
-                len = (p_rx-(char*)&rx_buff[rx_cnt_out]);
-                if( cmd != -1 )
-                {
-                    at_data_analysis( &cmd , &rx_buff[rx_cnt_out] , len );
-                    at_cmd_send( &cmd , &rx_buff[rx_cnt_out] , len );
-                }
-                else
-                {
-                    cmd = at_cmd_analysis( &rx_buff[rx_cnt_out] , len );
-                }
-                rx_cnt_out = (uint16_t)(p_rx-(char*)rx_buff)+2;
+							
+							len = (p_rx-(char*)&rx2_buff[rx2_cnt_out]);				
+							
+								if( len != 0 )
+								{
+/*
+									for( i=len-1 ; i>0 ; i-- )
+									{
+										if( rx2_buff[rx2_cnt_out+i] == '\r' )
+										{
+											len--;
+										}
+									}		
+*/								memset( buf , 0 , 100 );
+									uart1_log( "\r\n" , 2  );
+									uart1_log( &rx2_buff[rx2_cnt_out] , len  );
+									uart1_log( "\r\n" , 2  );
+									memcpy( buf , &rx2_buff[rx2_cnt_out] , len  );
+
+									if( cmd != -1 )
+									{
+											//at_data_analysis( &cmd , &rx2_buff[rx2_cnt_out] , len );
+											//at_cmd_send( &cmd , &rx2_buff[rx2_cnt_out] , len );
+											at_data_analysis( &cmd , buf , len );
+											at_cmd_send( &cmd , buf , len );
+									}
+									else
+									{
+											//cmd = at_cmd_analysis( &rx2_buff[rx2_cnt_out] , len );
+											cmd = at_cmd_analysis( buf , len );
+									}
+								}
+                rx2_cnt_out = (uint16_t)(p_rx-(char*)&rx2_buff)+2;
             }
             else
             {
               break;
             }
         }
-        rx_cnt_in = 0;
-        rx_cnt_out = 0;
+				memset(rx2_buff,0,sizeof(rx2_buff));
+        rx2_cnt_in = 0;
+        rx2_cnt_out = 0;
     }
 }
 
@@ -148,6 +191,6 @@ static void my_delay(uint32_t x)
   uint32_t y;
   for(;x>0;x--)
   {
-    for(y=200;y>0;y--);
+    for(y=18000;y>0;y--);
   }
 }
--- at.h
+++ at.h
@@ -1,6 +1,5 @@
-#ifndef AT_H_
 #define AT_H_
-
+/*
 #define AT              "AT"
 #define AT_CGMI         "AT+CGMI"
 #define AT_UPSV         "AT+UPSV?"
@@ -15,23 +14,46 @@
 #define RESP_ERR_SIM "+CME ERROR: SIM failure"
 #define RESP_ERR_CFUN "+CME ERROR: operation not allowed"
 #define RESP_ERR_PIN "+CME ERROR: Incorrect password"
+*/
 
+#define AT              "AT"
+#define AT_IMEI					"AT+GSN"				//Request International Mobile Equipment Identity (IMEI)
+#define AT_CCID         "AT+CCID" 			//This command returns the ICCID (Integrated Circuit Card Identifier) number of the (U)SIM card.
+#define AT_MANU_ID			"AT+GMI"				//This command returns the manufacturer identification text.
+#define AT_RSSI					"AT+CSQ"				//The Execution Command returns the received signal strength indication <rssi> and the channel bit error
+#define AT_FLIGHT				"AT+CFUN=0"			//Minimum functionality mode. Both (U)SIM and RF functions are disabled.
+#define AT_NORMAL				"AT+CFUN=1"			//Full functionality mode (by default).
+
+
+#define AT_OPEN_GNSS					"AT+QGPS=1" 		//Open GNSS
+#define AT_QUERY_GPS					"AT+QGPSLOC?"
+#define AT_CLOSE_GNSS					"AT+QGPSEND" 		//Close GNSS
 
 typedef enum
 {
-    GET_AT,             //"AT\r\n"
-    GET_CGMI,           //"AT+CGMI\r\n" Text string identifying the manufacturer.
-    GET_UPSV,           //"AT+UPSV?\r\n" power saving configuration.
-    GET_CGSN,           //"AT+CGSN\r\n" Returns the International Mobile station Equipment Identity (IMEI) number.
-    GET_ICCID,          //"AT+CCID\r\n" Returns the ICCID (Integrated Circuit Card ID) of the SIM-card.
-    GET_CPIN,           //"AT+CPIN?\r\n" PIN
-    SET_UMNOPROF,       //Automatically configures the module to be compliant to the requirements of various Mobile Network Operators.
-    GET_CFUN,           //Selects the level of functionality <fun> in the MT.
-    SET_CFUN_1,          //Selects the level of functionality <fun> in the MT.
-    GET_CSQ,            //Returns the radio signal strength <signal_power> and <qual> from the MT.
-    CMD_LIST
+	GET_AT,
+	GET_MANU_ID,
+	GET_IMEI,
+	GET_ICCID,
+	SET_NORMAL,	
+	GET_RSSI,
+	//SET_FLIGHT,
+	
+	CMD_LIST
 }at_cmd_list_t;
 
-void at_handle(void);
+typedef enum
+{
+	STATE_POWER_ON,
+	STATE_GET_RSSI,
+	STATE_EN_STANDBY,
+}at_state_list_t;
 
-#endif
+extern at_state_list_t g_at_state;
+
+
+void uart2_at_handle(void);
+
+void at_cmd_rssi(void);
+
+void at_cmd_pwron(void);
